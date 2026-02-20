2667bc7bd1c6a889ebf01d918007b122e30e7199
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-02-20 11:08:12 +0530

    [Anushka Singhal] Add .Added Feature UC-4 Extended Units

---
+++ QuantityMeasurementApp/features/src/main/java/com/BridgeLabz/QuantityMeasurementApp/uc4_extended_units/LengthUnit.java
@@ -0,0 +1,27 @@
+package com.BridgeLabz.QuantityMeasurementApp.uc4_extended_units;
+
+
+public enum LengthUnit {
+
+
+    FEET(1.0),
+    INCH(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(0.393701 / 12.0);
+
+
+
+    private final double conversionFactorToFeet;
+
+
+
+    LengthUnit(double conversionFactorToFeet) {
+        this.conversionFactorToFeet = conversionFactorToFeet;
+    }
+
+
+
+    public double toFeet(double value) {
+        return value * conversionFactorToFeet;
+    }
+}
