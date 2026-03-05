17b55547a1fb7d99a31d495da08c0bf74a15dfdc
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-03-05 11:45:31 +0530

    [Anushka Singhal] Add .Added Generic quantity

---
+++ src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/IMeasurable.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.QuantityMeasurementApp.generic_quantity;
+
+public interface IMeasurable {
+    double getConversionFactor();
+    default double convertToBaseUnit(double value) {
+        return value * getConversionFactor();
+    }
+    default double convertFromBaseUnit(double baseValue) {
+        return baseValue / getConversionFactor();
+    }
+
+    String getUnitName();
+}
---
+++ src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/LengthUnit.java
@@ -0,0 +1,25 @@
+package com.BridgeLabz.QuantityMeasurementApp.generic_quantity;
+
+public enum LengthUnit implements IMeasurable {
+
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
+    private final double conversionFactor;
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+    @Override
+    public double getConversionFactor() {
+
+
+        return conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+
+        return name();
+    }
+}
